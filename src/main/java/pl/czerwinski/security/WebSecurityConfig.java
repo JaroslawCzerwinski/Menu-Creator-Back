c50da986c6ee7faf56e69bad10fa386ca0873eb9
JaroslawCzerwinski <44581966+JaroslawCzerwinski@users.noreply.github.com>
2020-06-10 22:57:16 +0200

    Update security jdbc Auth

--- src/main/java/pl/czerwinski/security/WebSecurityConfig.java
+++ src/main/java/pl/czerwinski/security/WebSecurityConfig.java
@@ -5,14 +5,12 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
 
 @EnableWebSecurity
 @Configuration
@@ -29,30 +27,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-		.httpBasic()
-		.and()
-		.csrf().disable()
-		.authorizeRequests()
-		.antMatchers("/users","/days","/recipes").authenticated();
-		
+		.httpBasic().and().csrf().disable()
+		.authorizeRequests().antMatchers("/users", "/days", "/recipes").authenticated();
+
 	}
-	
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		  auth.inMemoryAuthentication().withUser("user").password(passwordEncoder().encode("password")).roles("USER");
-		/*auth // tu jest problem 
-			.jdbcAuthentication()
-			.dataSource(dataSource)
-			.passwordEncoder(passwordEncoder())
-			.usersByUsernameQuery(
-					"SELECT username, password, enabled from users where username = ?")
-			.authoritiesByUsernameQuery(
-	                "SELECT u.username, a.authority " +
-	                        "FROM user_authorities a, users u " +
-	                        "WHERE u.username = ? " +
-	                        "AND u.id = a.user_id"
-	                    );*/
-		
+		auth
+		.jdbcAuthentication().dataSource(dataSource).passwordEncoder(passwordEncoder())
+				.usersByUsernameQuery(
+						"SELECT users.email AS username, users.password AS password, users.user_enabled as enabled "
+								+ "FROM users WHERE users.email= ?")
+				.authoritiesByUsernameQuery(
+						"SELECT users.email AS username, users.role AS role " + "FROM users WHERE users.email= ?");
+
 	}
 
 }
